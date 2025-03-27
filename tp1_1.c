8a3ffbe1810c47a4c11b80a7f831beb0efe1dc98
colman nicolas <colman.nicolas.17@hotmail.com>
2025-03-26 23:40:06 -0300

    tp1_1 listo

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    printf("hola mundo");
+int main() {
+   // printf("hola mundo\n");
+
+    long double meganumero = 76348956523;
+    long double *puntero = &meganumero;
+
+    printf("contenido variable: %.2Lf \n", meganumero);
+    printf("direccion de memoria almacenada por el puntero: %p\n", (void*)puntero);
+    printf("direccion de memoria de la variable: %p \n", (void*)&meganumero);
+    printf("direccion de memoria del puntero: %p \n", (void*)&puntero);
+    printf("tamaño de la variable: %zu \n", sizeof(meganumero));
+
+    return 0;
 }
